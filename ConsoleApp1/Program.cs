1e2d10a00e7497d572acc834462519cfad9aa950
abdelrahmansaqr929-spec <abdelrahmansaqr929@gmail.com>
2026-08-30 10:59:56 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region answer_01
+            int pages = 464;
+            bool isAvailable = true;
+            if (isAvailable && pages > 300)
+            {
+                Console.WriteLine("you can borrow this book ");
+            }
+            #endregion
+        }
+    }
+}
