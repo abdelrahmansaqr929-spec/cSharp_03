b7d30aa83d499249eb903d728577ca6b7264201e
abdelrahmansaqr929-spec <abdelrahmansaqr929@gmail.com>
2026-08-30 11:37:39 +0300

    answer_04

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -31,6 +31,14 @@
             #region answer_03
             string sizeLabkle =
             pages > 300 ? sizeLabkle = "Long Book" : sizeLabkle = "Short Book";
+            Console.WriteLine(sizeLabkle);
+            #endregion
+            #region answer_04
+            string[] books = { "Clean code", "The Pragmatic Programmer", "Refactoring" };
+            for (int i = 0; i < books.Length; i++)
+            {
+                Console.WriteLine($" {i+1}.{books[i]}");
+            }
 
             #endregion
 
